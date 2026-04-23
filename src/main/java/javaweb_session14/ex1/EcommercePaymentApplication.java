69ae2f68caf557a143c3fa90deadbc2e49d7670a
Son Nguyen <sonseo15072006@gmail.com>
2026-04-23 22:38:14 +0700

    Ex1

---
+++ src/main/java/javaweb_session14/ex1/EcommercePaymentApplication.java
@@ -0,0 +1,12 @@
+package javaweb_session14.ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercePaymentApplication.class, args);
+    }
+}
---
+++ src/main/java/javaweb_session14/ex1/model/entity/Order.java
@@ -0,0 +1,20 @@
+package javaweb_session14.ex1.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+
+    public Order() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
